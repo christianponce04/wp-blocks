4417a00bbc5973e2fed5c942419213eb937fa46c
Christian Ponce <christianponce04@gmail.com>
2025-02-12 11:09:48 +0800

    adding edit and save function for the name and bio

--- wp-content/plugins/team/build/team/index.asset.php
+++ wp-content/plugins/team/build/team/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '3eae6c8485d8b89280ea');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'c70f869bef6faf011c6c');
--- wp-content/plugins/team/src/team-member/index.js
+++ wp-content/plugins/team/src/team-member/index.js
@@ -1,11 +1,29 @@
 import { registerBlockType } from '@wordpress/blocks';
 import { __ } from '@wordpress/i18n';
+import Edit from "./edit"
+import Save from "./save"
 
 registerBlockType('block-course/team-member',{
 		title:__("Team Member","team-member"),
 		description: __("A team member item","team-member"),
 		icon:"admin-users",
 		parent:["block-course/team-members"],
-		edit: () => <p> Edit</p>,
-		save: () => <p> save</p>,
+		support: {
+			reusable: false,
+			html:false,
+		},
+		attributes:{
+			name: {
+				type:"string",
+				source:"html",
+				selector:"h4"
+			},
+			bio: {
+				type:"string",
+				source:"html",
+				selector:"p"
+			},
+		},
+		edit: Edit,
+		save: Save,
 })
