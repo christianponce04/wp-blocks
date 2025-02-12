5198125869267fa7fa640133a86845299fdf4375
Christian Ponce <christianponce04@gmail.com>
2025-02-12 08:59:57 +0800

    added boilerplate-template and team block

---
+++ wp-content/plugins/boilerplate-template/build/boilerplate-template/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'c014841ee1d40bc98847');
---
+++ wp-content/plugins/team/build/team/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '458d0fc6e8daff9ee2cf');
