10c0d20fc961c7ec04bcb700ce5e78bae0f9da2e
Christian Ponce <christianponce04@gmail.com>
2025-02-12 15:14:13 +0800

    adding imagePlaceholder Components

--- wp-content/plugins/team/build/team/index.asset.php
+++ wp-content/plugins/team/build/team/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '218a87a528aab40c7747');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '484bc0369a3ebb3e6f9b');
