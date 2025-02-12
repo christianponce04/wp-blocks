c16cd4b2bafa14bcb82f5a2bc0e0ce3196042d15
Christian Ponce <christianponce04@gmail.com>
2025-02-12 17:15:46 +0800

    update save function

--- wp-content/plugins/team/build/team/index.asset.php
+++ wp-content/plugins/team/build/team/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'ef95901e9ac991af65e6');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '7a6066ab03b020c22d73');
